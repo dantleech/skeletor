d64bebc0f8a26f71e0fbe3936076e5979ae58aac
dantleech <dan.t.leech@gmail.com>
2016-03-27 16:01:11 +0100

    Added cache

--- lib/Console/Command/UpdateCommand.php
+++ lib/Console/Command/UpdateCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 
 class UpdateCommand extends Command
 {
